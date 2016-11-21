f14f1115b183c9788a82b7219d1a12fb576bca50
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-11-21 09:46:55 -0500

    11/21. Q1 complete.

---
+++ src/Q1.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author schum0689
+ */
+public class Q1 {
+
+    // Create method
+    public static double circleArea(double radius) {
+        // Calculate area
+        double area = 3.14 * (Math.pow(radius, 2));
+        // Return answer to user
+        return area;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Create scanner for user input
+        Scanner input = new Scanner(System.in);
+        // Ask user to input a radius value
+        System.out.println("Input a radius value:");
+        // Store user's answer
+        double radius = input.nextDouble();
+
+        // Calculate area of circle using method
+        double area = circleArea(radius);
+
+        // Output answer to user
+        System.out.println("The circle's area is " + area);
+    }
+}
