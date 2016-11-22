3a19382d82c42af83a83a92482d90fc0d6fde0ff
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-11-22 09:55:23 -0500

    11/22. Q3 complete.

--- src/Q3.java
+++ src/Q3.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author schum0689
@@ -13,23 +12,31 @@ import java.util.Scanner;
 public class Q3 {
 
     // Create procedure-type method
-    public static void factors(int number){
-        
-        // FOR LOOP?
-        int factorList;
+    public static void factors(int number) {
+        // Loop to divide number
+        for (int x = 1; x < (number + 1); x++) {
+            // Divide number by x
+            int remainder = number % x;
+            // If number divides evenly by x, output it
+            if (remainder == 0) {
+                System.out.print(x + "  ");
+            }
+        }
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // Create scanner for user input
         Scanner input = new Scanner(System.in);
         // Ask user to enter a number
         System.out.println("Enter a number:");
         // Store user's number
         int number = input.nextInt();
-        
+
+        // Output factor list using method
+        factors(number);
     }
 }
