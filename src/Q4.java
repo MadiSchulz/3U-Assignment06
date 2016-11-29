ddf38ecc8a006b09a6807a021b720ddc24c07b28
madis <madis@192.168.0.25>
2016-11-28 20:52:09 -0500

    11/28. Q4 complete.

--- src/Q4.java
+++ src/Q4.java
@@ -12,12 +12,11 @@ import java.util.Scanner;
 public class Q4 {
 
     // Create function-type method
-    public static double compoundInterest(double P, double r, int n) {
-        // Calculate compound interest
-        double newBalance;
-        // Return answer to user
-        return newBalance;
-        // 
+    public static double compoundInterest(double P, double r, double n) {
+        // Calculate new balance
+        double B = P * Math.pow((1 + r/100), n);
+        // Return new balance to user
+        return B;
     }
 
     /**
@@ -27,9 +26,23 @@ public class Q4 {
 
         // Create scanner for user input
         Scanner input = new Scanner(System.in);
-        // Ask user to input the current balance
-        System.out.println("The current balance is:");
-        // Store the current balance
-        double balance = input.nextDouble();
+
+        // Ask user to enter the principle balance
+        System.out.println("Enter the intital principle amount:");
+        double P = input.nextDouble();
+
+        // Ask user for the interest rate
+        System.out.println("Enter the interest rate:");
+        double r = input.nextDouble();
+
+        // Ask user for the number of years
+        System.out.println("Enter the number of years:");
+        double n = input.nextDouble();
+
+        // Calculate new balance using method
+        double newBalance = compoundInterest(P, r, n);
+        
+        // Output new balance
+        System.out.println("New balance = " + newBalance);
     }
 }
