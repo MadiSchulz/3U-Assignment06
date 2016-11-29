33240d8ed168b445a06df89fcdceb47e607069d8
madis <madis@192.168.0.25>
2016-11-28 20:10:32 -0500

    11/28. Q6 complete.

--- src/Q6.java
+++ src/Q6.java
@@ -6,33 +6,37 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author madis
  */
 public class Q6 {
-// Create procedure-type method
-    public static int lastDigit(int integer){
-        // Pull integer apart
-        int digit = integer % 10; 
+
+    // Create function-type method
+    public static int lastDigit(int integer) {
+        // Isolate the last digit in integer
+        int digit = integer % 10;
+        // Return last digit to user
         return digit;
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         // Create scanner for user input
         Scanner input = new Scanner(System.in);
-        // Ask user for an ineger
+        // Ask user for an integer
         System.out.println("Please input an integer:");
         // Store user's integer
         int integer = input.nextInt();
-        
+
+        // Find last digit using method
         int lastDigit = lastDigit(integer);
-        
-        System.out.println(lastDigit);
-        
+
+        // Output the last digit
+        System.out.println("The last digit is " + lastDigit);
+
     }
 }
