56f08c06fbf79efe5cf0ae0cbad2005dfe36ee7f
madis <madis@DESKTOP-MA781AP>
2016-11-30 13:31:52 -0500

    11/30. Working on Q9 & Q10.

--- src/Q10.java
+++ src/Q10.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,8 +22,19 @@ public class Q10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Create scanner for user input
+        Scanner input = new Scanner(System.in);
+        
+        // Ask user for first string
+        System.out.println("Enter string 1:");
+        // Store string 1
+        String one = input.nextLine();
+        
+        // Ask user for second string
+        System.out.println("Enter string 2:");
+        // Store string 2
+        String two = input.nextLine();
         
         
-    }
-    
+    } 
 }
--- src/Q9.java
+++ src/Q9.java
@@ -1,9 +1,11 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author schum0689
@@ -11,17 +13,30 @@
 public class Q9 {
 
     // Create function-type method
-    public static boolean allDigitsOdd(int integer){
-        
-         
+    public static boolean allDigitsOdd(int integer) {
+        // Calculate the remainder of the integer
+        int remainder = integer % 10;
+        // If remainder is 0
+        if (remainder == 0) {
+            return false;
+        } else {
+            return true;
         }
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        // Create scanner for user input
+        Scanner input = new Scanner(System.in);
+        // Ask user for an integer
+        System.out.println("Please input an integer:");
+        // Store user's integer
+        int integer = input.nextInt();
         
-        
+        allDigitsOdd(integer);
+
     }
-    
 }
